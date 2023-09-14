ed3e2f070411c1ad58b84e602f036e0dc8a53aff
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-14 15:48:05 -0700

    1 binaries uploaded

--- DelOroApp/src/app/material/material.module.ts
+++ DelOroApp/src/app/material/material.module.ts
@@ -6,6 +6,8 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 const modules = [
   BrowserAnimationsModule,
@@ -14,6 +16,7 @@ const modules = [
   MatIconModule,
   MatListModule,
   MatSidenavModule,
+  MatFormFieldModule
 ];
 
 @NgModule({
