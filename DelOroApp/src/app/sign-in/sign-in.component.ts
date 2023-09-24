566dc05e45333189e2b0996e2749d3d5c9c5d996
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-23 18:26:04 -0700

    3 files modified, 1 added, 1 deleted

--- DelOroApp/src/app/sign-in/sign-in.component.ts
+++ DelOroApp/src/app/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { AccountService } from '../account.service';
+import { ApiService } from '../api.service';
+
+
 
 @Component({
   selector: 'app-sign-in',
@@ -7,9 +9,12 @@ import { AccountService } from '../account.service';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent {
-
-  s: object
-  constructor (_accountService : AccountService){
-    this.s = _accountService.getAllCats()
+  title = 'frontEnd';
+  message: any;
+  constructor(private apiService: ApiService) { };
+  ngOnInit() {
+      this.apiService.getMessage().subscribe(data => {
+          this.message = data;
+      });
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,26 +1,20 @@
-const express = require('express')
+const express = require('express');
+const app = express();
 
-const app = express()
+// handling CORS
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin",
+			"http://localhost:4200");
+	res.header("Access-Control-Allow-Headers",
+			"Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
-app.listen(8000, () => {
-  console.log('Server started!')
-})
+// route for handling requests from the Angular client
+app.get('/api/message', (req, res) => {
+	res.json({ message: 'Yo this works!' });
+});
 
-app.route('/api/cats/:name').get((req, res) => {
-  const requestedCatName = req.params['name']
-  res.send({ name: requestedCatName })
-})
-
-const bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.route('/api/cats').post((req, res) => {
-  res.send(201, req.body)
-})
-
-app.route('/api/cats/:name').put((req, res) => {
-  res.send(200, req.body)
-})
-
-app.route('/api/cats/:name').delete((req, res) => {
-  res.sendStatus(204)
-})
+app.listen(3000, () => {
+	console.log('Server listening on port 3000');
+});
