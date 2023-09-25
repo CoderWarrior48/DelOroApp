97734a810d070440763b854e58f4aec1915f48b1
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-25 15:35:58 -0700

    3 files modified

--- server/server.js
+++ server/server.js
@@ -6,18 +6,23 @@ const app = express();
 const port = 3000;
 
 // Where we will keep books
-let accounts = [
+let pages = [
   {
     html:`
+
     <h1>H1 Heading</h1>
-    <p>p text</p>
+    <mat-card>
+      <mat-card-content>
+        <p> p text </p>
+      </mat-card-content>
+    </mat-card>
     `
   },
   {
-    user:"dawson"
+    html:"dawson"
   },
   {
-    user: "DH"
+    html: "DH"
   }
 ];
 
@@ -31,14 +36,14 @@ app.post('/book', (req, res) => {
   const book = req.body;
 
   // Output the book to the console for debugging
-  console.log(accounts);
-  books.push(accounts);
+  console.log(pages);
+  books.push(pages);
 
   res.send('Book is added to the database');
 });
 
 app.get('/', (req, res) => {
-  res.json(accounts);
+  res.json(pages);
 });
 
 app.listen(port, () => console.log(`DelOroApp server listening on port ${port}`));
