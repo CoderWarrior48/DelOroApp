2f826aea8ed0276108224a5f86eb44c68bd5fce2
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-25 07:57:36 -0700

    2 files modified

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 // create express app
 const app = express();
 // Setup server port
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 // parse requests of content-type - application/json
@@ -11,6 +11,9 @@ app.use(bodyParser.json())
 // define a root/default route
 app.get('/', (req, res) => {
   res.json({"message": "Hello World"});
+});
+app.get('/accounts', (req, res) => {
+  res.json({"codewarrior": "password1234"});
 });
 // listen for requests
 app.listen(port, () => {
