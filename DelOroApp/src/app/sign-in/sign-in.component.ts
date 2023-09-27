6c4da1c3184566e9cfb2e92f776785197fbe552f
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-27 12:01:17 -0700

    2 files modified

--- DelOroApp/src/app/sign-in/sign-in.component.ts
+++ DelOroApp/src/app/sign-in/sign-in.component.ts
@@ -9,7 +9,7 @@ import { Observable, take } from 'rxjs';
 })
 export class SignInComponent {
 
-  postReturn: any;//Observable<any> = new Observable<any>;
+  postReturn$: Observable<any> = new Observable<any>;
   usernameInput: string
   passwordInput: string
   
@@ -20,8 +20,8 @@ export class SignInComponent {
   }
   signIn() {
     console.log('posting...')
-    this.postReturn = this.apiService.postDataToServer()
-    console.log(this.postReturn)
+    this.postReturn$ = this.apiService.postDataToServer().pipe(take(1))
+    console.log(this.postReturn$)
   }
 
   // ngOnInit() {
