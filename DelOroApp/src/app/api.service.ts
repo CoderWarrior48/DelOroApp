704fa59015c2f7ff3d6d5f496dd1c8305e4d6ea8
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-09-27 12:10:56 -0700

    1 file modified

--- DelOroApp/src/app/api.service.ts
+++ DelOroApp/src/app/api.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 
 
 @Injectable({
@@ -10,7 +12,12 @@ export class ApiService {
   getDataFromServer(route: any) {
     return this.http.get(`http://localhost:3000${route}`);
   }
-  postDataToServer() {
-    return this.http.post(`http://localhost:3000/accounts`,{username: 'diego', password: '3412'});
+  // postDataToServer() {
+  //   return this.http.post(`http://localhost:3000/accounts`,{username: 'diego', password: '3412'});
+  // }
+
+  //testing....
+  postDataToServer(): Observable<any[]> {
+    return this.http.get<any[]>(`http://localhost:3000/accounts`).pipe(map(expenses => {return expenses}));
   }
 }
