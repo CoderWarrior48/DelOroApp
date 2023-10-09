356bc777dd0ed6ef05ea260a078dbf4a02b51ac0
CoderWarrior48 <77896436+CoderWarrior48@users.noreply.github.com>
2023-10-09 08:13:29 -0700

    3 files modified

--- DelOroApp/src/app/components/quizzes/quizzes.component.ts
+++ DelOroApp/src/app/components/quizzes/quizzes.component.ts
@@ -27,18 +27,11 @@ export class QuizzesComponent {
 export class FormEditor {
 
   addField() { 
+    this.fields.push({title:'title', type: 'text', placeholder:'placeholder'})
   }
 
   fields = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
+    {title:'title', type: 'text', placeholder:'placeholder'},
   ];
 
   drop(event: CdkDragDrop<string[]>) {
