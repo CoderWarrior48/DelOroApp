b6504f16a9c4878bb5bb98e319197219089275e6
CoderWarrior48 <dawson@hamera.com>
2023-11-09 08:25:22 -0800

    2 changes

--- server/files/newsfeed.js
+++ server/files/newsfeed.js
@@ -4,8 +4,8 @@ const newsFeed = [
     tags: '#academic',
     title:'Connect with a Peer Mentor',
     subtitle:'Peer Mentors are here to help!',
-    avatar:'../../assets/images/do-logo.svg',
-    image: '../../assets/images/eagle-head.png',
+    avatar:'../../assets/images/do-opener.jpg',
+    image: 'https://github.com/CoderWarrior48/DelOroApp/raw/FullstackTransition/DelOroApp/src/app/assets/images/do-opener.jpg',
     content: `
     If you have any questions about Del Oro, need help connecting to clubs, tutors, mental health specialists, or just want to talk, the Peer Mentors are here to help! Use <a href="https://docs.google.com/forms/d/e/1FAIpQLSeKu0nirxqJlKFkPDSKiX1T3i3sLrjo2Qsn1sV4JLuzz1UaUw/viewform?usp=sf_link">this referral form</a> for you or someone else, and we will reach out ASAP! All requests are strictly confidential.  
     `
